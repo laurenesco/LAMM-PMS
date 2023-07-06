0697dabd2fccb4ace8a512081c907fd5c45450c6
eliseomrico <eliseomrico@gmail.com>
2023-07-05 20:52:06 -0500

    Added the main menu form to stage for development

---
+++ main_menu_form/main_menu.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace LAMM_PMS
+{
+    public partial class main_menu : Form
+    {
+        public main_menu()
+        {
+            InitializeComponent();
+        }
+    
+    }
+}
