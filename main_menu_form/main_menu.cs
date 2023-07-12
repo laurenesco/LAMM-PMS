9da49fcecbdb024cf8e431e1a4a7882f3f843266
eliseomrico <eliseomrico@gmail.com>
2023-07-11 23:55:32 -0500

    Amended the main_menu form for min window sizing, renamed form components, removed superflous controls, and implemented color changing for selected buttons

--- main_menu_form/main_menu.cs
+++ main_menu_form/main_menu.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,10 +15,65 @@ namespace LAMM_PMS
 {
     public partial class main_menu : Form
     {
+        
+        // Initialize variable for the button that is currently selected
+        private Button selectedButton;
+
+        // Initialize back color variables for all buttons
+        private Color  originalColor = Color.FromArgb(23, 53, 53);
+        private Color selectedColor = Color.FromName("OliveDrab");
+
         public main_menu()
         {
             InitializeComponent();
+            selectedButton = main_menu_btn_home;
         }
-    
+
+        private void resetButtonColor() {
+            selectedButton.BackColor = originalColor;
+        }
+
+
+        private void setButtonColorToSelectedColor(Button button)
+        {
+            resetButtonColor();
+            button.BackColor = selectedColor;
+            selectedButton = button;
+        }
+
+        private void main_menu_btn_home_Click(object sender, EventArgs e)
+        {
+            setButtonColorToSelectedColor((Button)sender);
+        }
+
+        private void main_menu_btn_reservations_Click(object sender, EventArgs e)
+        {
+            setButtonColorToSelectedColor((Button)sender);
+        }
+
+        private void main_menu_btn_room_availability_Click(object sender, EventArgs e)
+        {
+            setButtonColorToSelectedColor((Button)sender);
+        }
+
+        private void main_menu_btn_guests_Click(object sender, EventArgs e)
+        {
+            setButtonColorToSelectedColor((Button)sender);
+        }
+
+        private void main_menu_btn_check_in_Click(object sender, EventArgs e)
+        {
+            setButtonColorToSelectedColor((Button)sender);
+        }
+
+        private void main_menu_btn_check_out_Click(object sender, EventArgs e)
+        {
+            setButtonColorToSelectedColor((Button)sender);
+        }
+
+        private void main_menu_btn_administrator_Click(object sender, EventArgs e)
+        {
+            setButtonColorToSelectedColor((Button)sender);
+        }
     }
 }
