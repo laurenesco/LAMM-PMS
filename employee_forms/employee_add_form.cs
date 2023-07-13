1f8f6b6c0296ef2e9e77f58f24280f9923cf54b0
lescobedo4@mail.stmarytx.edu <89123282+laurenesco@users.noreply.github.com>
2023-07-12 22:13:48 -0500

    Added employee edit form, standardized control naming

--- employee_forms/employee_add_form.cs
+++ employee_forms/employee_add_form.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-namespace LAMM_PMS
+namespace LAMM_PMS.employee_forms
 {
-    public partial class Employees : UserControl
+    public partial class employee_add_form : UserControl
     {
-        public Employees()
+        public employee_add_form()
         {
             InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ employee_forms/employee_edit_form.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LAMM_PMS
+{
+    public partial class employee_edit_form : UserControl
+    {
+        public employee_edit_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
